4aa7e3925e4ca1943e8c1a07965a979080263633
Paweł Kołaczyński <kolaczyn@protonmail.com>
2021-06-15 16:21:10 +0200

    cleanup

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,3 @@
-import { Button } from '@chakra-ui/button';
 import { CurrentStatsContainer } from '../containers/CurrentStatsContainer';
 import { NutrientsInputContainer } from '../containers/NutrientsInputContainer';
 import styles from '../styles/Home.module.css';
@@ -7,7 +6,6 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <main className={styles.main}>
-        <Button>Hello world</Button>
         <CurrentStatsContainer />
         <NutrientsInputContainer />
       </main>
