1667697e4c21d7b1f19427e7e027687a92da850e
Paweł Kołaczyński <kolaczyn@protonmail.com>
2021-06-15 19:51:18 +0200

    add footer

---
+++ components/FooterComponent.tsx
@@ -0,0 +1,53 @@
+import { Box, HStack, Text, VStack } from '@chakra-ui/react';
+import React from 'react';
+import { AiFillGithub as GithubIcon } from 'react-icons/ai';
+import { FiTwitter as TwitterIcon } from 'react-icons/fi';
+import { MdWeb as WebsiteIcon } from 'react-icons/md';
+// import {AiOmu} from 'react-icons/ai'
+import {AiOutlineMail as MailIcon } from 'react-icons/ai'
+
+import { FooterLinkComponent } from './FooterLinkComponent';
+
+interface FooterComponentProps {}
+
+export const FooterComponent: React.FC<FooterComponentProps> = ({}) => {
+  return (
+    <Box
+      boxShadow="lg"
+      zIndex="9001"
+      bg="gray.900"
+      mt="4"
+      px="4"
+      py="2"
+      w="100%"
+      color="white"
+      position="sticky"
+    >
+      <VStack spacing={3}>
+        <Text>created by Paweł Kołaczyński</Text>
+        <HStack spacing={10}>
+          <FooterLinkComponent
+            Icon={GithubIcon}
+            href="https://github.com/kolaczyn"
+          >
+            GitHub
+          </FooterLinkComponent>
+          <FooterLinkComponent
+            Icon={TwitterIcon}
+            href="https://twitter.com/kolaczyn"
+          >
+            Twitter
+          </FooterLinkComponent>
+          <FooterLinkComponent shouldOpenInNewTab={false} Icon={MailIcon} href="mailto:kolaczyn@protonmail.com">
+            Email
+          </FooterLinkComponent>
+          <FooterLinkComponent Icon={WebsiteIcon} href="https://kolaczyn.com">
+            My Website
+          </FooterLinkComponent>
+        </HStack>
+      </VStack>
+    </Box>
+  );
+};
+
+// 12 minutes left
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,25 @@
 import type { AppProps } from 'next/app';
-import { ChakraProvider, Container } from '@chakra-ui/react';
+import { Box, ChakraProvider, Container, Flex, Spacer } from '@chakra-ui/react';
 
 import { Provider } from 'react-redux';
 
 import store from '../redux/store';
 import { NavbarComponent } from '../components/NavbarComponent';
+import { FooterComponent } from '../components/FooterComponent';
+import React from 'react';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
       <ChakraProvider>
-        <NavbarComponent />
-        <Container>
-        <Component {...pageProps} />
-        </Container>
+        <Flex direction="column" minH="100vh">
+          <NavbarComponent />
+          <Container>
+            <Component {...pageProps} />
+          </Container>
+          <Spacer />
+          <FooterComponent />
+        </Flex>
       </ChakraProvider>
     </Provider>
   );
