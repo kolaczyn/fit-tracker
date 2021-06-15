341bedcd420caec58a868e58b27f2711ef2237f7
Paweł Kołaczyński <kolaczyn@protonmail.com>
2021-06-15 16:06:31 +0200

    display current stats

--- components/CurrentStatsComponent.tsx
+++ components/CurrentStatsComponent.tsx
@@ -1,19 +1,33 @@
-import { StatLabel, StatNumber } from '@chakra-ui/react';
+import { Grid, GridItem, StatLabel, StatNumber } from '@chakra-ui/react';
 import { Stat } from '@chakra-ui/stat';
 import React from 'react';
 import { Nutrients } from '../customTypes';
 
 interface CurrentStatsComponentProps {
-  nutrients: Nutrients;
+  nutrients: Nutrients<number>;
 }
+
+// I'm using this in order to make TypeScript happy
+const keysOfNutrients: Array<keyof Nutrients<number>> = [
+  'calories',
+  'fat',
+  'carbs',
+  'protein',
+];
 
 export const CurrentStatsComponent: React.FC<CurrentStatsComponentProps> = ({
   nutrients,
 }) => {
   return (
-    <Stat>
-      <StatLabel>Calories</StatLabel>
-      <StatNumber>{nutrients.calories}</StatNumber>
-    </Stat>
+    <Grid templateColumns="repeat(4, 1fr)" gap={4}>
+      {keysOfNutrients.map(nutrientName => (
+        <GridItem>
+          <Stat>
+            <StatLabel>{nutrientName}</StatLabel>
+            <StatNumber>{nutrients[nutrientName]}</StatNumber>
+          </Stat>
+        </GridItem>
+      ))}
+    </Grid>
   );
 };
