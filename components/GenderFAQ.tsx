cbdb6b1846265c3fc90bbb410acc4904d68878f9
Paweł Kołaczyński <kolaczyn@protonmail.com>
2021-06-16 23:51:55 +0200

    improve gender section

---
+++ components/GenderFAQ.tsx
@@ -0,0 +1,38 @@
+import { Accordion } from '@chakra-ui/accordion';
+import {
+  AccordionItem,
+  AccordionButton,
+  Box,
+  AccordionIcon,
+  AccordionPanel,
+} from '@chakra-ui/react';
+import React from 'react';
+
+export const GenderFAQ: React.FC = ({}) => {
+  return (
+    <Accordion allowMultiple>
+      <AccordionItem>
+        <h2>
+          <AccordionButton>
+            <Box textColor="gray.400" flex="1" textAlign="left">
+              Why are you asking for my gender?
+            </Box>
+            <AccordionIcon />
+          </AccordionButton>
+        </h2>
+        <AccordionPanel pb={4}>
+          <p>
+            Men have more muscles than women, they both have a different body
+            structure, which changes the way burn fat.
+          </p>
+          <p>
+            Because of this, the formulas for <em>Body Fat</em> and{' '}
+            <em>TDEE</em> are quite different and the former needs quite a few
+            more measurements for women. BMI calculation for men and women is
+            the same.
+          </p>
+        </AccordionPanel>
+      </AccordionItem>
+    </Accordion>
+  );
+};
