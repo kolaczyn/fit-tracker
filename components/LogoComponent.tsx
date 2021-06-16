a325248880948a3bc9031e8dd0b648f12f94862b
Paweł Kołaczyński <kolaczyn@protonmail.com>
2021-06-16 22:40:34 +0200

    update logo, make it a link

--- components/LogoComponent.tsx
+++ components/LogoComponent.tsx
@@ -1,8 +1,19 @@
 import { Text } from '@chakra-ui/layout';
 import React from 'react';
+import NextLink from 'next/link';
+import { HStack } from '@chakra-ui/react';
 
 interface LogoComponentProps {}
 
 export const LogoComponent: React.FC<LogoComponentProps> = ({}) => {
-  return <Text fontSize="3xl">Fit Tracker</Text>;
+  return (
+    <NextLink href="/">
+      <a>
+        <HStack fontSize="3xl">
+          <Text fontWeight="black">Track</Text>
+          <Text fontWeight="medium">Fit</Text>
+        </HStack>
+      </a>
+    </NextLink>
+  );
 };
