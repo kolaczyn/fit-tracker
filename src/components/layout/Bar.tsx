248c64544c07925cf41d9d150e7f95daf27317c8
Paweł Kołaczyński <kolaczyn@protonmail.com>
2021-06-20 17:28:06 +0200

    create generic box container to fit current color scheme

--- src/components/layout/Bar.tsx
+++ src/components/layout/Bar.tsx
@@ -1,14 +1,11 @@
-import { useColorModeValue } from '@chakra-ui/color-mode';
-import { Box, BoxProps } from '@chakra-ui/layout';
+import { BoxProps } from '@chakra-ui/layout';
 import React from 'react';
+import { AppBox } from '../ui/AppBox';
 
 export const Bar: React.FC<BoxProps> = ({ children, ...props }) => {
-  const bg = useColorModeValue('purple.50', 'gray.800');
-  const color = useColorModeValue('gray.900', 'white');
-  const borderColor = useColorModeValue('green.100', 'gray.900');
   return (
-    <Box borderTop="1px" borderBottom="1px" borderColor={borderColor} opacity="95%" boxShadow="lg" bg={bg} color={color} px="4" py="2" w="100%" {...props}>
+    <AppBox opacity="95%" boxShadow="lg" w="100%" px="4" py="2" {...props}>
       {children}
-    </Box>
+    </AppBox>
   );
 };
--- src/components/sidebar/WaterTracker.tsx
+++ src/components/sidebar/WaterTracker.tsx
@@ -1,25 +1,28 @@
 import { Box, Button, Heading, Text } from '@chakra-ui/react';
 import React, { useState } from 'react';
+import { AppBox } from '../ui/AppBox';
 
 interface WaterTrackerProps {}
 
 // 8 glasses per day (about 2 liters)
-const WATER_GOAL = 8
+const WATER_GOAL = 8;
 
 export const WaterTracker: React.FC<WaterTrackerProps> = ({}) => {
   const [glassesDrank, setGlassesDrank] = useState(0);
 
-  const hitGoalMessage = glassesDrank >= 8 ? 'Good job! 👍' : 'Try to hit 8.'
+  const hitGoalMessage = glassesDrank >= 8 ? 'Good job! 👍' : 'Try to hit 8.';
   const incrementGlasses = () => setGlassesDrank(old => old + 1);
   return (
-    <Box p="2" bg="gray.800">
+    <AppBox>
       <Heading fontSize="3xl" borderBottomWidth="1px" mb="1">
         Water Tracker
       </Heading>
-      <Text my="2">You drank {glassesDrank} glasses of water today. {hitGoalMessage}</Text>
+      <Text my="2">
+        You drank {glassesDrank} glasses of water today. {hitGoalMessage}
+      </Text>
       <Button colorScheme="blue" onClick={incrementGlasses}>
         Drink a glass of water
       </Button>
-    </Box>
+    </AppBox>
   );
 };
