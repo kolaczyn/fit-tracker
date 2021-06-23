81d648d907ea4ec3bb0122a3e6fe2f33eecbd3f2
Paweł Kołaczyński <kolaczyn@protonmail.com>
2021-06-23 09:01:15 +0200

    change default color scheme to dark

--- src/theme.ts
+++ src/theme.ts
@@ -1,8 +1,9 @@
-import { extendTheme } from '@chakra-ui/react';
+import { extendTheme, ThemeConfig } from '@chakra-ui/react';
 
-const theme = extendTheme({
+const config: ThemeConfig = {
   initialColorMode: 'dark',
   useSystemColorMode: false,
-});
+};
+const theme = extendTheme({ config });
 
 export default theme;
